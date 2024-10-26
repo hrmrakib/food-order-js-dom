78ef6856d67c0c687fbdfd78b2d7be8b3eea4f04
hrmrakib <hrmrakibs@gmail.com>
2024-10-26 08:00:01 +0600

    designed navbar with cart drawer

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"], // Define 'inter' as a custom font family
+      },
+    },
+  },
+};
